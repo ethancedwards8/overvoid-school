80e5c3d54604b115be18143b0f1dba81da19c6e8
Ryan Cole <le.mimikyu52@gmail.com>
2021-11-12 15:03:24 -0500

    working on inheritance, made serializable

---
+++ Assets/Scripts/planet_classes/p01.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p01 : SpaceTile
+{
+    bool canVisit = false;
+    hasMiniGame = false;
+    locked = false;
+    goalTile = false;
+    visited = false;
+
+    tileID;
+
+    description;
+    npcList;
+    tileResources;
+    difficulty = 1.0f;
+    fuelCost = 1;
+
+}
