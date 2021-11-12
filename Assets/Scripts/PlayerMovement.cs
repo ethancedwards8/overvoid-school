b9ea85e360c75f68326d874233af6aae586fdf57
Ryan Cole <le.mimikyu52@gmail.com>
2021-11-12 13:57:52 -0500

    Added whiteline for git test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private GameObject currentMap;
     private Boundaries currentMapBoundaries;
+
     [SerializeField] GameObject currentTile;
 
     void Start()
