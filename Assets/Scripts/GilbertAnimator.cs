1f1a448301e333409f4f556814a7c85e401ad688
Ethan Carter Edwards <ethan@ethancedwards.com>
2021-11-18 14:02:26 -0500

    Move Coroutine to the Start function instead of Update

--- Assets/Scripts/GilbertAnimator.cs
+++ Assets/Scripts/GilbertAnimator.cs
@@ -8,7 +8,7 @@ public class GilbertAnimator : MonoBehaviour
     [SerializeField] float animationSpeed;
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
         StartCoroutine(animate());
     }
